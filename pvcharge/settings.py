5788fbcc67906b97b2afdd0e228d20e9300ba2a8
peniel <tosalabuzz@gmail.com>
2025-07-27 09:22:29 +0200

    add ip

--- pvcharge/settings.py
+++ pvcharge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&q))hhj2uox^+&f5j#+$!a@e$!a2pf&drv9n(iyr&_=m-t65-n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.247.75.111','127.0.0.1']
 
 
 # Application definition
